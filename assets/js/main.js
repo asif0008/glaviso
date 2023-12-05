93c940dc061e2062fc244d53c93833794a0342b2
Asif Zulfiqar <asifzulfiqar43@gmail.com>
2023-12-05 15:00:09 +0500

    css changes made

--- assets/js/main.js
+++ assets/js/main.js
@@ -304,3 +304,20 @@ $(function() {
       });
     $('[href=#]').click(function(){return false});
   });
+
+$(function() {  
+    $('.frm-submit-btn')
+      .on('mouseenter', function(e) {
+              var parentOffset = $(this).offset(),
+                relX = e.pageX - parentOffset.left,
+                relY = e.pageY - parentOffset.top;
+              $(this).find('i').css({top:relY, left:relX})
+      })
+      .on('mouseout', function(e) {
+              var parentOffset = $(this).offset(),
+                relX = e.pageX - parentOffset.left,
+                relY = e.pageY - parentOffset.top;
+          $(this).find('i').css({top:relY, left:relX})
+      });
+    $('[href=#]').click(function(){return false});
+  });
