e952666a95c2a825b765a7adb1802f413de412d6
Asif Zulfiqar <asifzulfiqar43@gmail.com>
2023-12-05 13:53:51 +0500

    changes has been made on buttons, responsivness of the page and header

--- assets/js/main.js
+++ assets/js/main.js
@@ -287,3 +287,20 @@ $(function() {
       });
     $('[href=#]').click(function(){return false});
   });
+
+$(function() {  
+    $('.custom-btn-blue')
+      .on('mouseenter', function(e) {
+              var parentOffset = $(this).offset(),
+                relX = e.pageX - parentOffset.left,
+                relY = e.pageY - parentOffset.top;
+              $(this).find('span').css({top:relY, left:relX})
+      })
+      .on('mouseout', function(e) {
+              var parentOffset = $(this).offset(),
+                relX = e.pageX - parentOffset.left,
+                relY = e.pageY - parentOffset.top;
+          $(this).find('span').css({top:relY, left:relX})
+      });
+    $('[href=#]').click(function(){return false});
+  });
