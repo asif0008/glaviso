4f9f353c879bd31b396acede768e53f0f34572d1
Asif Zulfiqar <asifzulfiqar43@gmail.com>
2023-12-05 12:27:39 +0500

    css update for responsiveness

--- assets/js/main.js
+++ assets/js/main.js
@@ -270,4 +270,20 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
-
+// Button js
+$(function() {  
+    $('.custom-btn')
+      .on('mouseenter', function(e) {
+              var parentOffset = $(this).offset(),
+                relX = e.pageX - parentOffset.left,
+                relY = e.pageY - parentOffset.top;
+              $(this).find('span').css({top:relY, left:relX})
+      })
+      .on('mouseout', function(e) {
+              var parentOffset = $(this).offset(),
+                relX = e.pageX - parentOffset.left,
+                relY = e.pageY - parentOffset.top;
+          $(this).find('span').css({top:relY, left:relX})
+      });
+    $('[href=#]').click(function(){return false});
+  });
